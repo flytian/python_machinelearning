804ce89a93561847b4b013eaaa96871ea9a87ac7
tyf <flytonus@sina.cn>
2017-12-15 20:23:41 +0800

    the code of chapter 3

--- Chapter_3/Chapter_3_1_1_1.py
+++ Chapter_3/Chapter_3_1_1_1.py
@@ -1,4 +1,4 @@
-# coding = utf-8
+# coding:utf-8
 
 # 定义一组字典列表，用来表示多个数据样本（每个字典代表一个数据样本）。
 measurements = [{'city': 'Dubai', 'temperature': 33.}, {'city': 'London', 'temperature': 12.},
@@ -11,7 +11,7 @@ vec = DictVectorizer()
 # 输出转化之后的特征矩阵。
 print vec.fit_transform(measurements).toarray()
 # 输出各个维度的特征含义。
-print vec.get_feature_names()
+print vec.get_feature_names() # ['city=Dubai', 'city=London', 'city=San Fransisco', 'temperature']
 
 # 从sklearn.datasets里导入20类新闻文本数据抓取器。
 from sklearn.datasets import fetch_20newsgroups
